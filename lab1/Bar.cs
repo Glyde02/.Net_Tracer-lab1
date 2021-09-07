a7d7c5e6bd136be83ca8e31c59528c7bce082350
Glyde <anton123454321045@yandex.by>
2021-09-07 22:55:33 +0300

    Initial skeleton

---
+++ lab1/Bar.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    class Bar
+    {
+        private ITracer _tracer;
+
+        internal Bar(ITracer tracer)
+        {
+            _tracer = tracer;
+        }
+
+        public void InnerMethod()
+        {
+            _tracer.StartTrace();
+
+            _tracer.StopTrace();
+        }
+    }
+}
---
+++ lab1/Tracer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    struct TraceResult
+    {
+        public string methodName;
+        public string className;
+        public int time;
+    }
+
+    class Tracer : ITracer
+    {
+        private List<TraceResult> traceResult = new List<TraceResult>();
+
+        // вызывается в начале замеряемого метода
+        public void StartTrace()
+        {
+
+        }
+
+        // вызывается в конце замеряемого метода 
+        public void StopTrace()
+        {
+        }
+
+        // получить результаты измерений  
+        public List<TraceResult> GetTraceResult()
+        {
+            return traceResult;
+        }
+    }
+    
+}
